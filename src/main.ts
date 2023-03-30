32ff61499e0193e5ca9130f86446ef7f2ba5f69c
egorageev <egor.ageev@exex.com>
2023-03-30 23:12:55 +0300

    fix cors?

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Публичное API')
     .setDescription('Лабораторная работа №5')
